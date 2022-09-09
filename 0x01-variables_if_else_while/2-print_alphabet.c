6e3df8a55d0083938c32c9eb18a9f43bf4099417
Elphaz <elphazshiferaw@gmail.com>
2022-09-09 13:41:59 -0700

    0 to 9

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: A program that prints the alphabet in lowercase, followed by a new line.
+ * Description: A program that prints the alphabet in lowercase.
  * Return: 0
  */
 int main(void)
